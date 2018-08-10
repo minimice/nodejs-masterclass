6f8b4df83db054dca2aff14d64f03adfa3db48cb
Chooi-Guan Lim <chooi-guan.lim@scania.com>
2018-08-10 16:11:55 +0200

    Add rest app

---
+++ rest-app/index.js
@@ -0,0 +1,36 @@
+// API
+
+const http = require('http');
+const url = require('url');
+
+// Respond to requests with string
+const server = http.createServer(function(req,res) {
+
+	// Parse URL
+	const parsedUrl = url.parse(req.url, true);
+
+	// Get path of URL
+	const path = parsedUrl.pathname;
+	const trimmedPath = path.replace(/^\/+|\/+$/g,'');
+
+	// Get query string as object
+	var queryStringObject = parsedUrl.query;
+
+	// Get http method
+	const method = req.method.toLowerCase();
+
+	// Get headers as an object
+	const headers = req.headers;
+
+	// Send response
+	res.end('Hello World\n');
+
+	// Log request path
+	console.log('Request received with headers', headers);
+});
+
+// Start server listen to port 3000
+server.listen(3100, function() {
+	console.log('Server listening on port 3100');
+});
+
